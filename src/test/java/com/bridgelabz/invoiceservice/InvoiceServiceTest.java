c7ee992020cb2832dd8f1237f0d1da466fe814ff
DESKTOP-1260D35\Hp <deoraumesh32@gmail.com>
2020-10-26 22:09:30 +0530

    [Umesh]Added: test code to get invoice by user id

--- src/test/java/com/bridgelabz/invoiceservice/InvoiceServiceTest.java
+++ src/test/java/com/bridgelabz/invoiceservice/InvoiceServiceTest.java
@@ -43,4 +43,16 @@ public class InvoiceServiceTest {
 	}
 
 
+	@Test
+	public void givenUserIdAndRides_ShouldReturnInvoiceSummary() {
+		String userId="abc";
+		Ride[] rides = {new Ride(2.0,5),
+				new Ride(0.1,1)
+				};
+		invoiceService.addRides(userId, rides);
+		InvoiceSummary summary = invoiceService.getInvoiceSummary(userId);
+		InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2,30.0);
+		Assert.assertEquals(expectedInvoiceSummary,summary);
+	}
+
 }
